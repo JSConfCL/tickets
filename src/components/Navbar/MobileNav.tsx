9cc4f983df9e786d301adccfc6b3b54923d1e580
VadokDev <gonzalo.fdez06@gmail.com>
2023-08-12 21:15:01 -0400

    removed hamburguer menu margin

--- src/components/Navbar/MobileNav.tsx
+++ src/components/Navbar/MobileNav.tsx
@@ -18,7 +18,7 @@ export function MobileNav({ items }: NavBarProps) {
       <SheetTrigger asChild>
         <Button
           variant="ghost"
-          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+          className="px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
         >
           <Menu className="h-5 w-5" />
           <span className="sr-only">Toggle Menu</span>
