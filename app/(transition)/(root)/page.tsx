4ab3e59349b3bd2c49034033f600de056bf2a4fb
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-11-19 18:07:24 -0800

    update

--- app/(transition)/(root)/page.tsx
+++ app/(transition)/(root)/page.tsx
@@ -1,6 +1,15 @@
+import { getApolloClient } from "../../../src/api/ApolloClient";
 import { LandingPageEvents } from "../../../src/components/features/LandingPageEvents";
+import {
+  FetchExampleEventsDocument,
+  FetchExampleEventsQuery,
+} from "../../../src/components/features/LandingPageEvents/graphql/FetchExampleEvents.generated";
 
-export default function Home() {
+export default async function Home() {
+  const c = getApolloClient();
+  const variable = await c.query<FetchExampleEventsQuery>({
+    query: FetchExampleEventsDocument,
+  });
   return (
     <main className="flex max-w-5xl flex-col items-center justify-between px-6 pt-36 transition-all md:px-10 md:pt-44 xl:px-0 xl:pt-52">
       <div className="flex flex-col gap-16 pb-4">
@@ -9,6 +18,11 @@ export default function Home() {
         </h1>
       </div>
       <div className="flex flex-col gap-4">
+        <h1>RSC</h1>
+        {variable.data?.events?.map((event) => <div>{event.id}</div>)}
+      </div>
+      <div className="flex flex-col gap-4">
+        <h1>Client fetching</h1>
         <LandingPageEvents />
       </div>
     </main>
