ce1bd307ef65544da044d5ac6cb4dfb9891cd828
felipe torres <felipe.torressepulveda@gmail.com>
2024-06-22 09:55:27 -0700

    Handling reauthentication/token refresh flow on graphql errors

--- src/api/ApolloWrapper.tsx
+++ src/api/ApolloWrapper.tsx
@@ -1,32 +1,161 @@
 "use client";
 import {
   ApolloClient,
+  ApolloLink,
   ApolloProvider,
   HttpLink,
   InMemoryCache,
+  from,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+import { RetryLink } from "@apollo/client/link/retry";
 import cookies from "js-cookie";
 
-import { COOKIE_NAME } from "../utils/supabase/client";
+import { useSetTokenRef, useTokenRef } from "../utils/supabase/AuthProvider";
+import { COOKIE_NAME, supabaseClient } from "../utils/supabase/client";
 
-const authLink = setContext((_, context) => {
-  // get the authentication token from local storage if it exists
-  const token = cookies.get(COOKIE_NAME) ?? null;
-  const headers = (context.headers as Record<string, string>) ?? {};
-  if (token) {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${token}`,
-      },
-    };
-  }
-  // return the headers to the context so httpLink can read them
-  return {
-    headers,
-  };
+const retryLink = new RetryLink();
+
+// Este link "promueve" errores de graphql a errores de Javascript.
+// El retry link los va a procesar y reintentar :)
+// Importante ponerlo entre el retryLink y el httpLink
+const errorPromotionLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((data) => {
+    if (data && data.errors && data.errors.length > 0) {
+      throw new Error("GraphQL Operational Error. Retriable");
+    }
+    return data;
+  });
 });
+
+// Este link se encarga de añadir el token de autenticación a las peticiones
+const useAuthLink = () => {
+  const accessToken = useTokenRef().current;
+  return setContext((_, context) => {
+    // Obtenemos el access token desde el contexto.
+    const token = accessToken;
+    const headers = (context.headers as Record<string, string>) ?? {};
+    // TODO: Considerar si es necesario validar que el JWT sea valido/parseable
+    if (token) {
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${token}`,
+        },
+      };
+    }
+    // Si no hay token, retornamos los headers sin modificar
+    return {
+      headers,
+    };
+  });
+};
+
+const useErrorLink = () => {
+  const setToken = useSetTokenRef();
+  return onError(({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        if (err.extensions?.code === "UNAUTHENTICATED") {
+          supabaseClient.auth
+            .getSession()
+            .then(({ data, error }) => {
+              if (error) {
+                // eslint-disable-next-line no-console
+                console.error("Error refreshing access token", error);
+                return;
+              }
+
+              const newToken = data.session?.access_token;
+              if (!newToken) {
+                // eslint-disable-next-line no-console
+                console.error("No access token found in session data");
+              } else {
+                setToken(newToken);
+              }
+              forward(operation);
+            })
+            .catch((error) => {
+              // eslint-disable-next-line no-console
+              console.error("Error refreshing access token", error);
+            });
+          // }
+        }
+      }
+    } else if (networkError) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `[Network error]: ${networkError.name} - ${networkError.message}. stack: ${networkError.stack}`,
+      );
+    } else {
+      // eslint-disable-next-line no-console
+      console.error("Unknown error", operation);
+      forward(operation);
+    }
+  });
+};
+
+// const checkAndRefreshTokenLink = new ApolloLink((operation, forward) => {
+//   try {
+//     const undecodedToken = cookies.get(COOKIE_NAME) ?? null;
+//     console.log("undecodedToken", undecodedToken);
+//     if (!undecodedToken) {
+//       return forward(operation);
+//     }
+//     const token = jwtDecode(undecodedToken);
+//     console.log("TOKEN", token);
+//     if (!token || !token.exp) {
+//       return forward(operation);
+//     }
+//     // Refresh token if it's expired using supabaseclient (and enqueue the operation to be retried after refreshing the token)
+//     const now = Date.now() / 1000;
+//     if (token.exp - now < 60 * 5) {
+//       operationQueue.push(operation);
+//       return forward(operation);
+//     } else {
+//       return forward(operation);
+//     }
+//   } catch (error) {
+//     console.error("Error checking token", error);
+//     return forward(operation);
+//   }
+
+//   // if (!token) {
+//   //   // Token does not exist, refresh it
+//   //   supabase.auth.refreshAccessToken().then(({ data, error }) => {
+//   //     if (error) {
+//   //       console.error("Error refreshing access token", error);
+//   //       observer.error(error);
+//   //       return;
+//   //     }
+
+//   //     const newToken = data.session.access_token;
+//   //     cookies.set(COOKIE_NAME, newToken);
+
+//   //     const oldHeaders = operation.getContext().headers;
+//   //     operation.setContext({
+//   //       headers: {
+//   //         ...oldHeaders,
+//   //         authorization: `Bearer ${newToken}`,
+//   //       },
+//   //     });
+
+//   //     forward(operation).subscribe({
+//   //       next: (result) => observer.next(result),
+//   //       error: (networkError) => observer.error(networkError),
+//   //       complete: () => observer.complete(),
+//   //     });
+//   //   });
+//   // } else {
+//   //   // Token exists, proceed with the operation
+//   //   forward(operation).subscribe({
+//   //     next: (result) => observer.next(result),
+//   //     error: (networkError) => observer.error(networkError),
+//   //     complete: () => observer.complete(),
+//   //   });
+//   // }
+// });
 
 function useMakeClient() {
   const httpLink = new HttpLink({
@@ -41,11 +170,19 @@ function useMakeClient() {
     // to an Apollo Client data fetching hook, e.g.:
     // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
   });
-
+  const authLink = useAuthLink();
+  const errorLink = useErrorLink();
   return new ApolloClient({
     // use the `NextSSRInMemoryCache`, not the normal `InMemoryCache`
     cache: new InMemoryCache(),
-    link: authLink.concat(httpLink),
+    link: from([
+      // checkAndRefreshTokenLink,
+      retryLink,
+      errorPromotionLink,
+      errorLink,
+      retryLink,
+      authLink.concat(httpLink),
+    ]),
     credentials: "include",
   });
 }
--- src/utils/supabase/AuthProvider/index.tsx
+++ src/utils/supabase/AuthProvider/index.tsx
@@ -1,7 +1,16 @@
 "use client";
-import { User } from "@supabase/supabase-js";
+import { Session, User } from "@supabase/supabase-js";
 import cookies from "js-cookie";
-import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import {
+  MutableRefObject,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 import {
   COOKIE_NAME,
@@ -13,41 +22,49 @@ export type AuthContextType = {
   user: User | null;
   isLogged: boolean;
   isReady: boolean;
+  tokenRef: MutableRefObject<string | null>;
+  setTokenRef: (token: string | null) => void;
 };
 
 export const AuthContext = createContext<AuthContextType>({
   user: null,
   isLogged: false,
   isReady: false,
+  tokenRef: { current: null },
+  setTokenRef: () => {},
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [supabaseSession, setSession] = useState<Session | null>(null);
   const [user, setUser] = useState<User | null>(null);
   const [isReady, setIsReady] = useState(false);
+  const tokenRef = useRef<string | null>(null);
+
+  const setTokenRef = useCallback((token: string | null) => {
+    tokenRef.current = token;
+    if (!tokenRef.current) {
+      cookies.remove(COOKIE_NAME);
+    } else {
+      cookies.set(COOKIE_NAME, tokenRef.current, getCookieOptions());
+    }
+  }, []);
 
   useEffect(() => {
     const initialize = async () => {
       const {
         data: { session },
       } = await supabaseClient.auth.getSession();
-      if (session) {
-        setUser(session?.user ?? null);
-        const accessToken = session?.access_token ?? null;
-        cookies.set(COOKIE_NAME, accessToken, getCookieOptions());
-      }
-      setIsReady(true);
+      setSession(session);
+      await supabaseClient.auth.startAutoRefresh();
     };
 
     const {
       data: { subscription },
-    } = supabaseClient.auth.onAuthStateChange((_event, session) => {
-      const access_token = session?.access_token ?? null;
-      if (!access_token) {
-        cookies.remove(COOKIE_NAME);
-      } else {
-        cookies.set(COOKIE_NAME, access_token, getCookieOptions());
-      }
-      setUser(session?.user ?? null);
+    } = supabaseClient.auth.onAuthStateChange(async (_event, session) => {
+      setSession(session);
+      const token = session?.access_token ?? null;
+      tokenRef.current = token;
+      await supabaseClient.auth.startAutoRefresh();
     });
 
     // eslint-disable-next-line no-console
@@ -55,15 +72,35 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     return () => subscription.unsubscribe();
   }, []);
 
+  useEffect(() => {
+    if (supabaseSession) {
+      const { user } = supabaseSession;
+      setIsReady(true);
+      setUser(user);
+      if (!tokenRef.current) {
+        cookies.remove(COOKIE_NAME);
+      } else {
+        cookies.set(COOKIE_NAME, tokenRef.current, getCookieOptions());
+      }
+    }
+  }, [supabaseSession]);
+
   const value = useMemo(
-    () => ({ user, isLogged: Boolean(user?.id), isReady }),
-    [user, isReady],
+    () => ({
+      user,
+      isLogged: Boolean(user?.id),
+      isReady,
+      tokenRef,
+      setTokenRef,
+    }),
+    [user, isReady, setTokenRef],
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export const useUser = () => useContext(AuthContext).user;
-
+export const useTokenRef = () => useContext(AuthContext).tokenRef;
+export const useSetTokenRef = () => useContext(AuthContext).setTokenRef;
 export const useIsLoggedIn = () => useContext(AuthContext).isLogged;
 export const useIsAuthReady = () => useContext(AuthContext).isReady;
