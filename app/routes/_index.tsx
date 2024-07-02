78d69cc50baf441dace2bb617f04893335d94fe5
Hector Vergara <hvergara@users.noreply.github.com>
2024-07-01 23:49:21 -0400

    Restore prettier (#38)
    Restores prettier:ci command. Also adds a `prettier:fix` command (for
    development purposes)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,5 @@
 export default function Index() {
-
-
-
   return (
-
-    
     <div className="p-4 font-sans">
       <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
       <ul className="mt-4 list-disc space-y-2 pl-6">
