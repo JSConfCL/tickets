4bdbd9c93c74569ecd2ed2177473f3546da95a5d
felipe torres <felipe.torressepulveda@gmail.com>
2024-07-21 20:35:59 -0700

    prettier

--- app/components/Location/Location.tsx
+++ app/components/Location/Location.tsx
@@ -10,7 +10,7 @@ export const Location = ({ title, location }: LocationType) => {
 
   return (
     <section className="flex w-full flex-col gap-4 bg-slate-900 p-6 dark:bg-slate-50">
-      <h2 className="text-xl text-slate-50 dark:text-slate-900 md:text-4xl">
+      <h2 className="text-xl text-slate-50 md:text-4xl dark:text-slate-900">
         {title}
       </h2>
       {/* <GoogleMapsEmbed
