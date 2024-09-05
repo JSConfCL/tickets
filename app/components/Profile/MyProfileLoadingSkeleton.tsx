8795041e94369605b249ee888acf09867f0ecace
Jose Lezama <me@joseglego.io>
2024-09-05 04:36:09 -0400

    feat(Profile): include Cal as new font

--- app/components/Profile/MyProfileLoadingSkeleton.tsx
+++ app/components/Profile/MyProfileLoadingSkeleton.tsx
@@ -1,23 +1,27 @@
-import { Button } from "~/components/ui/button";
+import { Avatar, AvatarFallback } from "~/components/ui/avatar";
 import { Card } from "~/components/ui/card";
+import { Separator } from "~/components/ui/separator";
 import { Skeleton } from "~/components/ui/skeleton";
 
 const LoadingCard = () => {
   return (
-    <Card className="flex w-full max-w-2xl flex-col gap-3 bg-gray-900/40 p-5">
-      <div className="flex flex-col gap-1 text-xs text-gray-400">
-        <span className="flex items-center gap-1 capitalize">
-          <Skeleton className="h-3 w-40" />
-        </span>
-        <span className="flex items-center gap-1 capitalize">
-          <Skeleton className="h-3 w-20" />
-        </span>
-      </div>
-      <Skeleton className="h-8 w-36" />
-      <div>
-        <Button asChild variant="secondary">
-          <Skeleton className="h-9 w-32" />
-        </Button>
+    <Card className="mx-auto w-full max-w-[460px] p-4">
+      <div className="flex flex-col items-center gap-8">
+        <Avatar className="aspect-square size-24">
+          <AvatarFallback className="text-4xl uppercase" />
+        </Avatar>
+        <div className="flex flex-col items-center gap-2">
+          <Skeleton className="h-6 w-40" />
+          <Skeleton className="h-6 w-20" />
+          <Skeleton className="h-6 w-32" />
+        </div>
+        <Separator className="sm:max-w-md" />
+        <div className="w-full">
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-6 w-64" />
+            <Skeleton className="h-6 w-40" />
+          </div>
+        </div>
       </div>
     </Card>
   );
@@ -25,8 +29,6 @@ const LoadingCard = () => {
 export const MyProfileLoadingSkeleton = () => {
   return (
     <div className="flex flex-col gap-6">
-      <LoadingCard />
-      <LoadingCard />
       <LoadingCard />
     </div>
   );
