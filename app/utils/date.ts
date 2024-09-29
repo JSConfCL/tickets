bcfedee73ba3c23656f050adf0ac0eda4edf9ef7
Jose Lezama <me@joseglego.io>
2024-09-29 12:13:22 -0300

    chore(libs): more accurate to figma

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,5 +1,8 @@
 import { formatDistanceToNow, subDays, format } from "date-fns";
 
+const capitalizeFirstLetter = (string: string): string =>
+  string.charAt(0).toUpperCase() + string.slice(1);
+
 export const formatDate = (date: string | Date | null | undefined): string => {
   if (date == null) {
     return "";
@@ -10,9 +13,23 @@ export const formatDate = (date: string | Date | null | undefined): string => {
   const relativeDate = formatDistanceToNow(parsedDate, {
     addSuffix: true,
   });
-  const formattedDate = format(parsedDate, "cccc - d 'de' MMMM, yyyy");
+  const formattedDate = capitalizeFirstLetter(
+    format(parsedDate, "EEEE d 'de' MMMM, yyyy"),
+  );
 
   return isOverAWeekFromNow ? formattedDate : relativeDate;
+};
+
+export const formatCalendarDate = (
+  date: string | Date | null | undefined,
+): string => {
+  if (date == null) {
+    return "";
+  }
+
+  const parsedDate = date instanceof Date ? date : new Date(date);
+
+  return format(parsedDate, "dd/MM/yyyy");
 };
 
 export const formatTime = (date: string | Date | null | undefined): string => {
