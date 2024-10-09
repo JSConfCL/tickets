79ce83cf683a9252e609105b5f59ea34ccb95196
Jose Lezama <me@joseglego.io>
2024-10-09 07:08:33 -0300

    fix(TMP): remove magic link form

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -15,7 +15,7 @@ export const Login = () => {
   }, []);
 
   return (
-    <div className="flex w-full max-w-lg shrink-0 flex-col gap-4 rounded-2xl border bg-background p-10 shadow-xl">
+    <div className="flex w-full max-w-lg shrink-0 flex-col gap-4 rounded-2xl border bg-background p-10 shadow-xl [&_form]:hidden">
       <div>
         <h1 className="text-left text-2xl font-semibold">Regístrate.</h1>
         <p className="text-left text-sm text-muted-foreground">
@@ -44,6 +44,10 @@ export const Login = () => {
         }}
         appearance={{
           theme: ThemeSupa,
+          extend: true,
+          className: {
+            divider: "!hidden",
+          },
           variables: {
             default: {
               colors: {
