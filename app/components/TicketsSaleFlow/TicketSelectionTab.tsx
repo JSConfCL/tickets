ad88dc58f39b6e4c5383db7c346797d309920645
Jose Lezama <me@joseglego.io>
2024-11-30 11:49:43 -0300

    feat(TicketSaleFlow): better UI

--- app/components/TicketsSaleFlow/TicketSelectionTab.tsx
+++ app/components/TicketsSaleFlow/TicketSelectionTab.tsx
@@ -1,11 +1,12 @@
 import { Minus, Plus } from "lucide-react";
+import { useState } from "react";
 
 import {
   TicketTemplateStatus,
   TicketTemplateVisibility,
 } from "~/api/gql/graphql";
-import { Button } from "~/components/ui/button";
-import { Card, CardContent } from "~/components/ui/card";
+import { Button, buttonVariants } from "~/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import {
   Table,
   TableBody,
@@ -15,11 +16,92 @@ import {
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
+import { cn } from "~/utils/utils";
 
 import { EventTicketFragmentFragment } from "./graphql/EventTicketFragment.generated";
 import { TicketAmountInput } from "./inputs";
 import { FirstStepFooter } from "./Stepper";
 import { Step, TicketsState } from "./types";
+
+const MobileCard = ({
+  item,
+  getFormmatedTicketPrice,
+  selectedTickets,
+  onMinusButtonClick,
+  onPlusButtonClick,
+  onInputChange,
+}: {
+  item: EventTicketFragmentFragment;
+  getFormmatedTicketPrice: (
+    ticket: EventTicketFragmentFragment,
+  ) => string | null;
+  selectedTickets: TicketsState;
+  onMinusButtonClick: (ticketId: string) => void;
+  onPlusButtonClick: (ticketId: string) => void;
+  onInputChange: (ticketId: string, amount: number) => void;
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <Card className="w-full">
+      <CardHeader className="cursor-pointer p-4 pb-0">
+        <CardTitle className="flex items-center justify-between">
+          <div className="flex flex-col gap-2">
+            <div>{item.name}</div>
+            <div>
+              {" "}
+              {item.isFree || !item.prices?.length
+                ? "Gratis"
+                : getFormmatedTicketPrice(item)}
+            </div>
+          </div>
+          {item.status === TicketTemplateStatus.Active ? (
+            <div className="flex w-[100px] flex-row items-center justify-between font-medium">
+              <Button
+                className="size-[20px] shrink-0 rounded-full p-0 "
+                disabled={!selectedTickets[item.id]}
+                onClick={() => onMinusButtonClick(item.id)}
+              >
+                <Minus size={14} />
+                <span className="sr-only">Remover Entradar</span>
+              </Button>
+              <div className="w-8">
+                <TicketAmountInput
+                  value={selectedTickets[item.id] ?? 0}
+                  onChange={(e) => {
+                    onInputChange(item.id, parseInt(e.target.value, 10));
+                    // handleChange(item.id, parseInt(e.target.value, 10));
+                  }}
+                />
+              </div>
+              <Button
+                className="size-[20px] shrink-0 rounded-full p-0"
+                size="sm"
+                onClick={() => onPlusButtonClick(item.id)}
+              >
+                <Plus size={14} />
+                <span className="sr-only">Agregar Entradar</span>
+              </Button>
+            </div>
+          ) : (
+            <span className="font-medium">No Disponible</span>
+          )}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="p-4">
+        <button
+          className={cn(buttonVariants({ variant: "link" }), "p-0")}
+          onClick={() => {
+            setIsExpanded((isExpanded) => !isExpanded);
+          }}
+        >
+          {isExpanded ? "Esconder Detalles" : "Ver Detalles"}
+        </button>
+        {isExpanded && <p>{item.description}</p>}
+      </CardContent>
+    </Card>
+  );
+};
 
 export const TicketSelectionTab = ({
   getFormmatedTicketPrice,
@@ -55,7 +137,7 @@ export const TicketSelectionTab = ({
 }) => {
   return (
     <div className="flex flex-col gap-9">
-      <Card>
+      <Card className="hidden sm:block">
         <CardContent className="pb-2">
           <h2 className="mt-4	text-2xl font-bold leading-[52px]">
             Entrada General
@@ -78,63 +160,58 @@ export const TicketSelectionTab = ({
               </TableRow>
             </TableHeader>
             <TableBody>
-              {tickets
-                .filter(
-                  (ticket) =>
-                    ticket.visibility === TicketTemplateVisibility.Public,
-                )
-                .map((ticket) => (
-                  <TableRow key={ticket.id}>
-                    <TableCell className="p-4 py-6 text-center font-bold">
-                      {ticket.name}
-                    </TableCell>
-                    <TableCell className="py-6 text-center text-muted-foreground">
-                      {ticket.description}
-                    </TableCell>
-                    <TableCell className="py-6 text-center">
-                      {ticket.status === TicketTemplateStatus.Active ? (
-                        <div className="flex w-[100px] flex-row items-center justify-between gap-2 font-medium">
-                          <Button
-                            className="size-[20px] shrink-0 rounded-full p-0 "
-                            disabled={!selectedTickets[ticket.id]}
-                            onClick={() => onMinusButtonClick(ticket.id)}
-                          >
-                            <Minus size={14} />
-                            <span className="sr-only">Remover Entradar</span>
-                          </Button>
-                          <div className="w-16">
-                            <TicketAmountInput
-                              value={selectedTickets[ticket.id] ?? 0}
-                              onChange={(e) => {
-                                onInputChange(
-                                  ticket.id,
-                                  parseInt(e.target.value, 10),
-                                );
-                                // handleChange(ticket.id, parseInt(e.target.value, 10));
-                              }}
-                            />
-                          </div>
-                          <Button
-                            className="size-[20px] shrink-0 rounded-full p-0"
-                            size="sm"
-                            onClick={() => onPlusButtonClick(ticket.id)}
-                          >
-                            <Plus size={14} />
-                            <span className="sr-only">Agregar Entradar</span>
-                          </Button>
+              {tickets.map((ticket) => (
+                <TableRow key={ticket.id}>
+                  <TableCell className="p-4 py-6 text-center font-bold">
+                    {ticket.name}
+                  </TableCell>
+                  <TableCell className="py-6 text-center text-muted-foreground">
+                    {ticket.description}
+                  </TableCell>
+                  <TableCell className="py-6 text-center">
+                    {ticket.status === TicketTemplateStatus.Active ? (
+                      <div className="flex w-[100px] flex-row items-center justify-between gap-2 font-medium">
+                        <Button
+                          className="size-[20px] shrink-0 rounded-full p-0 "
+                          disabled={!selectedTickets[ticket.id]}
+                          onClick={() => onMinusButtonClick(ticket.id)}
+                        >
+                          <Minus size={14} />
+                          <span className="sr-only">Remover Entradar</span>
+                        </Button>
+                        <div className="w-16">
+                          <TicketAmountInput
+                            value={selectedTickets[ticket.id] ?? 0}
+                            onChange={(e) => {
+                              onInputChange(
+                                ticket.id,
+                                parseInt(e.target.value, 10),
+                              );
+                              // handleChange(ticket.id, parseInt(e.target.value, 10));
+                            }}
+                          />
                         </div>
-                      ) : (
-                        <span className="font-medium">No Disponible</span>
-                      )}
-                    </TableCell>
-                    <TableCell className="py-6 text-center font-bold">
-                      {" "}
-                      {ticket.isFree || !ticket.prices?.length
-                        ? "Gratis"
-                        : getFormmatedTicketPrice(ticket)}
-                    </TableCell>
-                  </TableRow>
-                ))}
+                        <Button
+                          className="size-[20px] shrink-0 rounded-full p-0"
+                          size="sm"
+                          onClick={() => onPlusButtonClick(ticket.id)}
+                        >
+                          <Plus size={14} />
+                          <span className="sr-only">Agregar Entradar</span>
+                        </Button>
+                      </div>
+                    ) : (
+                      <span className="font-medium">No Disponible</span>
+                    )}
+                  </TableCell>
+                  <TableCell className="py-6 text-center font-bold">
+                    {" "}
+                    {ticket.isFree || !ticket.prices?.length
+                      ? "Gratis"
+                      : getFormmatedTicketPrice(ticket)}
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
             <TableFooter>
               <TableRow className="bg-background hover:bg-background">
@@ -154,6 +231,30 @@ export const TicketSelectionTab = ({
           </Table>
         </CardContent>
       </Card>
+      <div className="space-y-8 sm:hidden">
+        <h2 className="mt-4	text-2xl font-bold leading-[52px]">
+          Entrada General
+        </h2>
+        <div className="space-y-4">
+          {tickets.map((item) => (
+            <MobileCard
+              key={item.id}
+              item={item}
+              getFormmatedTicketPrice={getFormmatedTicketPrice}
+              selectedTickets={selectedTickets}
+              onMinusButtonClick={onMinusButtonClick}
+              onPlusButtonClick={onPlusButtonClick}
+              onInputChange={onInputChange}
+            />
+          ))}
+        </div>
+        <div className="mt-12 flex justify-between gap-2">
+          <div className="text-lg font-bold uppercase">Total a Pagar</div>
+          <div className="text-lg font-bold">
+            {!numberOfTickets || formattedTotal ? formattedTotal : "Gratis"}
+          </div>
+        </div>
+      </div>
       <FirstStepFooter
         onClickNext={nextStep}
         isDisabled={numberOfTickets === 0 || !isActive || hasFinished}
--- app/components/TicketsSaleFlow/TicketsSaleFlowSkeleton.tsx
+++ app/components/TicketsSaleFlow/TicketsSaleFlowSkeleton.tsx
@@ -9,6 +9,33 @@ import {
   TableRow,
 } from "~/components/ui/table";
 
+const TableRowSkeletonTicket = () => (
+  <TableRow>
+    <TableCell className="p-4 text-center">
+      <Skeleton className="mx-auto h-4 w-24" />
+    </TableCell>
+    <TableCell className="text-center">
+      <Skeleton className="mx-auto h-4 w-96" />
+    </TableCell>
+    <TableCell className="text-center">
+      <Skeleton className="mx-auto h-4 w-12" />
+    </TableCell>
+    <TableCell className="text-center">
+      <Skeleton className="mx-auto h-4 w-24" />
+    </TableCell>
+  </TableRow>
+);
+
+const MobileSkeletonTicket = () => (
+  <Card className="w-full p-0">
+    <CardContent className="flex flex-col gap-2 p-4">
+      <Skeleton className="h-8 w-64" />
+      <Skeleton className="h-8 w-24" />
+      <Skeleton className="mt-2 h-8 w-32" />
+    </CardContent>
+  </Card>
+);
+
 export const TicketsSaleFlowSkeleton = () => {
   const tickets = Array.from({ length: 5 }).map((_, index) => index);
 
@@ -16,15 +43,24 @@ export const TicketsSaleFlowSkeleton = () => {
     <div className="mx-auto flex w-full max-w-[1076px] flex-col gap-9">
       <div className="flex flex-col gap-9">
         <Skeleton className="mx-auto h-20 w-full rounded-md md:h-40" />
-        <div className="flex w-full items-center justify-between">
+        <div className="flex flex-col items-center gap-4 md:flex-row">
           <div className="flex flex-col gap-4">
-            <Skeleton className="h-16 w-48" />
-            <Skeleton className="h-8 w-96" />
+            <h2 className="text-3xl font-semibold">Compra de tickets</h2>
+            <p className="text-muted-foreground">
+              Selecciona la cantidad de tickets que quieres comprar
+            </p>
           </div>
-          <Skeleton className="h-8 w-24" />
+          <div className="flex w-full flex-col items-end	justify-end gap-4">
+            <Skeleton className="h-8 w-full md:w-80" />
+            <Skeleton className="h-8 w-full md:w-64" />
+            <Skeleton className="h-8 w-full md:w-64" />
+          </div>
         </div>
-        <Card>
-          <CardContent>
+        <Card className="hidden sm:block">
+          <CardContent className="pb-2">
+            <h2 className="mt-4 text-2xl font-bold leading-[52px]">
+              Entrada General
+            </h2>
             <Table>
               <TableHeader>
                 <TableRow className="border-t hover:bg-transparent">
@@ -44,27 +80,29 @@ export const TicketsSaleFlowSkeleton = () => {
               </TableHeader>
               <TableBody>
                 {tickets.map((ticket: number) => (
-                  <TableRow key={ticket}>
-                    <TableCell className="p-4 text-center">
-                      <Skeleton className="mx-auto h-4 w-24" />
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Skeleton className="mx-auto h-4 w-96" />
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Skeleton className="mx-auto h-4 w-12" />
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Skeleton className="mx-auto h-4 w-24" />
-                    </TableCell>
-                  </TableRow>
+                  <TableRowSkeletonTicket key={ticket} />
                 ))}
               </TableBody>
             </Table>
           </CardContent>
         </Card>
+        <div className="space-y-8 sm:hidden">
+          <h2 className="mt-4	text-2xl font-bold leading-[52px]">
+            Entrada General
+          </h2>
+          <div className="space-y-4">
+            {tickets.map((ticket: number) => (
+              <MobileSkeletonTicket key={ticket} />
+            ))}
+          </div>
+          <div className="mt-12 flex justify-between gap-2">
+            <div className="text-lg font-bold uppercase">Total a Pagar</div>
+            <div className="text-lg font-bold">
+              <Skeleton className="h-8 w-32" />
+            </div>
+          </div>
+        </div>
         <div className="flex w-full flex-col justify-end gap-2 md:flex-row">
-          <Skeleton className="h-8 w-full md:w-32" />
           <Skeleton className="h-8 w-full md:w-32" />
         </div>
       </div>
